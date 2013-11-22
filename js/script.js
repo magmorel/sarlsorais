d9526aaa473ba0a10c8bd36ad68f443f2ce6eaf1
magmorel <magalimorel20@yahoo.fr>
2013-11-22 15:59:09 +0100

    intialisation

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	
+sfHover = function() {
+        var sfEls = document.getElementById("menu").getElementsByTagName("LI");
+        for (var i=0; i<sfEls.length; i++) {
+                sfEls[i].onmouseover=function() {
+                        this.className+=" sfhover";
+                }
+                sfEls[i].onmouseout=function() {
+                        this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+                } } }
+				
+				
+$('#navigation-Smart a').click(function(){  
+				$('#menu-smartphone').show(1);
+					});				
+    
+	
+			
+});
